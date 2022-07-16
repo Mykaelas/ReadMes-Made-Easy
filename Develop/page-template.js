db6412260c35008e63609a77e65a297f203086f5
Mykaela Saenz <mickycore@gmail.com>
2022-07-16 15:51:13 -0500

    added in a complete page template that will pull the information given by the user and apply it to the ReadMe

--- Develop/page-template.js
+++ Develop/page-template.js
@@ -1,19 +1,34 @@
-const generateReadMe = () => {
-    return `
+const generateReadMe = (data) => {
+    return ` # ${data.title}
 
     ## Table of Contents
     - [Description](#description)
     - [Installation](#installation)
     - [Usage](#usage)
+    - [License](#license)
     - [Contributions](#contribution)
-    - [Tests](#tests)
+    - [Features](#features)
     - [Questions](#questions)
+    - [Tests](#tests)
 
     ## Description:
-
-
+    ${data.description}
+    ## Installation:
+    ${data.installation}
+    ## Usage:
+    ${data.usage}
+    ## License:
+    ${data.license}
+    ## Contributions:
+    ${data.contribution}
+    ## Features:
+    ${data.features}
+    ## Questions:
+    ${data.questions}
+    ## Tests:
+    ${data.tests}
+   
     `;
   };
   module.exports = generateReadMe;
 
-//   entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
