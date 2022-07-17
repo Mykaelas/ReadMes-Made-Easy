058dc8640bee9c07ec1d4422a76a0431ac7bcd7f
Mykaela Saenz <mickycore@gmail.com>
2022-07-17 17:56:41 -0500

    added in question types for index.js

--- Develop/index.js
+++ Develop/index.js
@@ -20,7 +20,89 @@ const inquirer = require('inquirer');
 const generatePage = require('./page-template');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+   // This returns all of the user input to the const.
+    return inquirer.prompt([ 
+    
+    // Asking for the project name    
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?(Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid response!');
+                return false;
+            }
+        }
+    },
+    
+    // Asking if the user knows what license they're using
+    {
+        type: 'confirm',
+        name: 'confirmLicense',
+        message: 'Do you know what kind of License you are using?',
+        validate: confirmingLicense => {
+            if (confirmingLicense) {
+                return true;
+            } else {
+                console.log ('Check out https://choosealicense.com/ to help you decide on which license(s) to use. ')
+                return false;
+            } 
+        },
+    },
+    
+    // Asking for the user to select a license
+    {
+        
+        type: 'checkbox',
+        name: 'license',
+        message: 'What License(s) does your project fall under?',
+        choices: ['MIT License', 'Apache License 2.0', 'GPL License(aka GNU General Public License v3.0', 'BSD License(2-clause)', 'BSD License(3-clause)', 'BSD License(4-clause)', 'None of the above']
+    },
+    
+    // Asking the user for a description of their project
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project'
+    },
+    
+    // Asking the user for installation instructions
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Write the installation instructions for your project here'
+    },
+
+    // Asks the user to provide an example of their project/expected outcome as well as provide links if possible
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Write what the expected outcome of your project should be as well as paste example links if applicable."
+    },
+
+    // Asks about other contributers
+    {
+        type: 'confirm',
+        name: 'contribution',
+        message: 'Did other people contribute to your project?',
+        default: false
+    },
+
+    // Ask for other contributers information
+    {
+        type: 'input',
+        name: 'contributions',
+        message: ' Please list the name and githubs of your fellow contributers'
+    }
+
+
+]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/page-template.js
+++ Develop/page-template.js
@@ -1,5 +1,10 @@
+
+// This is used as the outline/template for creating the ReadMe. 
 const generateReadMe = (data) => {
     return ` # ${data.title}
+
+    ## License:
+    [![license](https://img.shields.io/badge/license-${data.license}-blue.svg)](https://img.shields.io/badge/license-${data.license}-blue.svg)
 
     ## Table of Contents
     - [Description](#description)
@@ -20,11 +25,13 @@ const generateReadMe = (data) => {
     ## License:
     ${data.license}
     ## Contributions:
-    ${data.contribution}
+    ${data.contribution}, ${data.contributions}
     ## Features:
     ${data.features}
     ## Questions:
     ${data.questions}
+        Want to see more of my work? Reach out to me here at https://github.com/[${data.username}] to see what I am working on next.
+        You can reach me at my email,[${data.email}] for any other questions you may have.
     ## Tests:
     ${data.tests}
    
